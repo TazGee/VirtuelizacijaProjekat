c096f33a6427d4a209f83d5e2173b1e0842d7ca1
TazGee <nikolatasic2004@gmail.com>
2026-05-07 11:07:52 +0200

    Uradjene neke stvari za batchove

--- Server/Events/EventArgs/WarningEventArgs.cs
+++ Server/Events/EventArgs/WarningEventArgs.cs
@@ -1,7 +1,28 @@
-﻿namespace Server.Events.EventArgs
+﻿namespace Server.Events
 {
-    internal class WarningEventArgs
+    public class WarningEventArgs : System.EventArgs
     {
+        public string WarningType { get; private set; }
+        public string CountryCode { get; private set; }
+        public int Hour { get; private set; }
+        public double Value { get; private set; }
+        public string Direction { get; private set; }
+        public string Message { get; private set; }
 
+        public WarningEventArgs(
+            string warningType,
+            string countryCode,
+            int hour,
+            double value,
+            string direction,
+            string message)
+        {
+            WarningType = warningType;
+            CountryCode = countryCode;
+            Hour = hour;
+            Value = value;
+            Direction = direction;
+            Message = message;
+        }
     }
 }
