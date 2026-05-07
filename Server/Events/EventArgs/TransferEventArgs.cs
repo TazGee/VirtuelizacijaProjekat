9584944958732f21394fc441fda79e16eb4d82d8
TazGee <nikolatasic2004@gmail.com>
2026-05-07 10:59:52 +0200

    Uradjen event args

--- Server/Events/EventArgs/TransferEventArgs.cs
+++ Server/Events/EventArgs/TransferEventArgs.cs
@@ -1,7 +1,18 @@
-﻿namespace Server.Events.EventArgs
-{
-    internal class TransferEventArgs
-    {
+﻿using System;
 
+namespace Server.Events
+{
+    public class TransferEventArgs : System.EventArgs
+    {
+        public string CountryCode { get; private set; }
+        public DateTime Date { get; private set; }
+        public string Message { get; private set; }
+
+        public TransferEventArgs(string countryCode, DateTime date, string message)
+        {
+            CountryCode = countryCode;
+            Date = date;
+            Message = message;
+        }
     }
 }
